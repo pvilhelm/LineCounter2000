e0e056f882a8c9c4e51f8179bbb8040b5aee8ff9
Petter Tomner <tomner@kth.se>
2017-05-22 18:09:51 +0200

    Made MainForm into manual instead of Designer bs

---
+++ LineCounter2000/MainForm.cs
@@ -0,0 +1,152 @@
+﻿namespace LineCounter2000
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+ 
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+
+            this.flowLayoutPanel_left = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel_left_top = new System.Windows.Forms.Panel();
+
+            this.currentDirTextBox = new System.Windows.Forms.TextBox();
+            this.openFolderButton = new System.Windows.Forms.Button();
+            this.checkedFileList = new LineCounter2000.CheckedFileList();
+            this.makeSelectedIntoProject = new System.Windows.Forms.Button();
+            
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.totUsedLinesTable = new System.Windows.Forms.Label();
+            this.totLinesLabel = new System.Windows.Forms.Label();
+            this.sessionTextBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+
+            //this.flowLayoutPanel_left.SuspendLayout();
+            //this.flowLayoutPanel_left_top.SuspendLayout();
+             
+            //this.SuspendLayout();
+
+            //Setup parent window first
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 1024);
+            
+            this.Name = "MainForm";
+            this.Text = "Line Counter 2000";
+
+            this.Controls.Add(this.flowLayoutPanel_left);
+
+            //Setup left flow layout
+            this.flowLayoutPanel_left.BackColor = System.Drawing.Color.DarkGreen;
+
+            this.flowLayoutPanel_left.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.flowLayoutPanel_left.Anchor |= System.Windows.Forms.AnchorStyles.Right;
+            this.flowLayoutPanel_left.Anchor |= System.Windows.Forms.AnchorStyles.Bottom;
+            this.flowLayoutPanel_left.Anchor |= System.Windows.Forms.AnchorStyles.Top;
+            this.flowLayoutPanel_left.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel_left.Dock |= System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel_left.Width = this.Width / 2;
+
+            this.flowLayoutPanel_left.ResumeLayout(false);
+            this.flowLayoutPanel_left.PerformLayout();
+
+            this.flowLayoutPanel_left.Controls.Add(this.flowLayoutPanel_left_top);
+            {
+                this.flowLayoutPanel_left_top.BackColor = System.Drawing.Color.Wheat;
+
+                this.flowLayoutPanel_left_top.Controls.Add(this.currentDirTextBox);
+                this.flowLayoutPanel_left_top.Controls.Add(this.openFolderButton);
+
+                this.flowLayoutPanel_left_top.Anchor = System.Windows.Forms.AnchorStyles.Left;
+                this.flowLayoutPanel_left_top.Anchor |= System.Windows.Forms.AnchorStyles.Top;
+                this.flowLayoutPanel_left_top.Anchor |= System.Windows.Forms.AnchorStyles.Right;
+                this.flowLayoutPanel_left_top.Height = 40;
+                this.flowLayoutPanel_left_top.Dock = System.Windows.Forms.DockStyle.Top;
+                
+                
+
+                //this.currentDirTextBox
+                {
+                    var ctrl = this.currentDirTextBox;
+                    ctrl.AutoSize = false;
+                    ctrl.Anchor = System.Windows.Forms.AnchorStyles.Top;
+                    ctrl.Anchor |= System.Windows.Forms.AnchorStyles.Left;
+                    ctrl.Dock = System.Windows.Forms.DockStyle.Left;
+                    ctrl.Width = (ctrl.Parent.Width - 200);
+                    ctrl.Height = ctrl.Parent.Height;
+                    ctrl.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+                    
+                    
+                }
+                {
+                    var ctrl = this.openFolderButton;
+                    ctrl.Anchor = System.Windows.Forms.AnchorStyles.Top;
+                    ctrl.Anchor |= System.Windows.Forms.AnchorStyles.Right;
+                    ctrl.Dock = System.Windows.Forms.DockStyle.Right;
+                    ctrl.Width = 200; 
+                }
+                //this.flowLayoutPanel_left_top.Anchor = System.Windows.Forms.AnchorStyles.Left;
+                //this.flowLayoutPanel_left_top.Anchor |= System.Windows.Forms.AnchorStyles.Top;
+                
+                //this.flowLayoutPanel_left_top.Dock = System.Windows.Forms.DockStyle.Top;
+                
+            }
+            /*this.flowLayoutPanel_left.Controls.Add(this.checkedFileList);
+
+            //checkedFileList
+            this.checkedFileList.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.checkedFileList.Anchor |= System.Windows.Forms.AnchorStyles.Right;
+            this.checkedFileList.Anchor |= System.Windows.Forms.AnchorStyles.Bottom;
+            */
+
+
+            
+
+
+            this.flowLayoutPanel_left_top.ResumeLayout(false);
+            this.flowLayoutPanel_left.ResumeLayout(false);
+
+            this.flowLayoutPanel_left_top.PerformLayout();
+            this.flowLayoutPanel_left.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+        private System.Windows.Forms.Panel flowLayoutPanel_left;
+        private System.Windows.Forms.Panel flowLayoutPanel_left_top;
+
+        private System.Windows.Forms.TextBox currentDirTextBox;
+        
+        private System.Windows.Forms.Button openFolderButton;
+        
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label totUsedLinesTable;
+        private System.Windows.Forms.Label totLinesLabel;
+        private System.Windows.Forms.Button makeSelectedIntoProject;
+        private CheckedFileList checkedFileList;
+        private System.Windows.Forms.TextBox sessionTextBox;
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
---
+++ LineCounter2000/MainForm_main.cs
@@ -0,0 +1,206 @@
+﻿ 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LineCounter2000
+{
+    public partial class MainForm : Form
+    {
+
+        Session session = new Session(@"c:\\");
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            //
+            this.currentDirTextBox.Text = session.curr_dir;
+            this.checkedFileList.setSession(ref session);
+            this.checkedFileList.UpdateList(session.curr_dir);
+
+        }
+
+        private void currentDirTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void currentDirTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                string tmp = session.curr_dir;
+                session.curr_dir = this.currentDirTextBox.Text;
+                try
+                {
+                    this.checkedFileList.UpdateList(session.curr_dir);
+                }
+                catch (Exception f)
+                {
+                    MessageBox.Show("Could not read directory " + session.curr_dir + "\n" + f.ToString());
+                    session.curr_dir = tmp;
+                    this.checkedFileList.UpdateList(session.curr_dir);
+                }
+            }
+        }
+
+        private void makeSelectedIntoProject_Click(object sender, EventArgs e)
+        {
+            fn_makeSelectedIntoProject();
+        }
+
+        private void fn_makeSelectedIntoProject()
+        {
+            CheckedFileList.ElementContainer s = (CheckedFileList.ElementContainer)this.checkedFileList.SelectedItem;
+            if (s.type == CheckedFileList.ElementContainer.ElementType.FOLDER)
+            {
+                session.addProject(s.path);
+                updateStates();
+            }
+            else
+            {
+                MessageBox.Show("Selected item is not a folder");
+            }
+        }
+
+        bool no_recursion_checkedFileList_ItemCheck = false;
+
+        private void checkedFileList_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+
+            if (no_recursion_checkedFileList_ItemCheck)
+                return;
+            else
+                no_recursion_checkedFileList_ItemCheck = true;
+
+            int i = e.Index;
+            CheckedFileList cfl = this.checkedFileList;
+            CheckedFileList.ElementContainer s = (CheckedFileList.ElementContainer)cfl.Items[i];
+
+            if (e.NewValue == CheckState.Unchecked && e.CurrentValue != CheckState.Unchecked)
+            {
+                this.session.removeFileFromProject(s.path);
+                updateStates();
+            }
+            else if (e.NewValue == CheckState.Checked && CheckState.Checked != e.CurrentValue && s.type == CheckedFileList.ElementContainer.ElementType.FILE)
+            {
+                this.session.addFileToProject(s.path);
+                updateStates();
+            }
+
+            no_recursion_checkedFileList_ItemCheck = false;
+
+
+        }
+
+        private void checkedFileList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void updateLabels()
+        {
+            int tot_lines = session.getTotLines();
+            int tot_used_lines = session.getTotUsedLines();
+            this.totLinesLabel.Text = tot_lines.ToString();
+            this.totUsedLinesTable.Text = tot_used_lines.ToString();
+            this.currentDirTextBox.Text = session.curr_dir;
+            this.sessionTextBox.Text = session.getTextRepresentation();
+        }
+
+        private void updateStates()
+        {
+            this.checkedFileList.UpdateList(session.curr_dir);
+            updateLabels();
+        }
+
+        private void checkedFileList_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            CheckedFileList chfl = (CheckedFileList)sender;
+
+            CheckedFileList.ElementContainer ec = (CheckedFileList.ElementContainer)chfl.SelectedItem;
+
+            if (ec == null)
+                return;
+
+
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+
+
+                if (ec.type == CheckedFileList.ElementContainer.ElementType.FOLDER || ec.type == CheckedFileList.ElementContainer.ElementType.PROJECT_FOLDER)
+                {
+                    string path = ec.path;
+
+                    if (path == "..")
+                    {
+                        path = session.curr_dir;
+                        Match m = Regex.Match(path, @"((\w:\\\\(.+\\)*)(.*\\))|(\w:\\\\)");
+                        if (m.Groups[2].Length != 0)
+                        {
+                            session.curr_dir = m.Groups[2].ToString();//parent folder
+                            chfl.ClearSelected();
+                            chfl.SetSelected(0, true);
+                        }
+                        else
+                            session.curr_dir = m.Groups[5].ToString();//root level (stay put)
+
+                    }
+                    else
+                    {
+                        session.curr_dir = path;
+                        chfl.ClearSelected();
+                        chfl.SetSelected(0, true);
+                    }
+                    updateStates();
+                }
+            }
+            else if (e.KeyChar == 'w' || e.KeyChar == 'W')
+            {
+                fn_makeSelectedIntoProject();
+            }
+            else if (e.KeyChar == 'e' || e.KeyChar == 'E')
+            {
+                if (ec.type == CheckedFileList.ElementContainer.ElementType.FILE)
+                {
+                    string path = ec.path;
+
+                    if (path == "..")
+                    {
+                        return; //dont select ".." (parent folder)
+                    }
+                    else
+                    {
+                        session.addFileToProject(path);
+                    }
+                    updateStates();
+                }
+            }
+            else if (e.KeyChar == 'q' || e.KeyChar == 'Q')
+            {
+                if (ec.type == CheckedFileList.ElementContainer.ElementType.INCLUDED_FILE)
+                {
+                    string path = ec.path;
+
+                    if (path == "..")
+                    {
+                        return; //dont select ".." (parent folder)
+                    }
+                    else
+                    {
+                        session.removeFileFromProject(path);
+                    }
+                    updateStates();
+                }
+            }
+        }
+    }
+}
